627cb26e6590a445b0242162a00f85bdd009a0f2
Perry Melange <isprotejesvalkata@gmail.com>
2025-02-21 12:24:16 +0100

    directoy restructure
    Signed-off-by: Perry Melange <isprotejesvalkata@gmail.com>

---
+++ labgrid/strategy/covrflashbootstrategy.py
@@ -0,0 +1,75 @@
+import enum
+
+import attr
+
+from labgrid.factory import target_factory
+from labgrid.strategy import Strategy, StrategyError
+
+
+class Status(enum.Enum):
+    unknown = 0
+    off = 1
+    on = 2
+    shell = 3
+    initialconfig = 4
+    config = 5
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class CovrFlashBootStrategy(Strategy):
+    """CovrFlashBootStrategy - Strategy to switch to boot from flash or shell"""
+    bindings = {
+        "power": "PowerProtocol",
+        'reset': "ButtonProtocol",
+        "console": "ConsoleProtocol",
+        "shell": "ShellDriver",
+        "config": "OpenwrtUciDriver",
+    }
+
+    status = attr.ib(default=Status.unknown)
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+
+    def transition(self, status):
+        if not isinstance(status, Status):
+            status = Status[status]
+        if status == Status.unknown:
+            raise StrategyError(f"can not transition to {status}")
+        elif status == self.status:
+            return # nothing to do
+        elif status == Status.off:
+            self.target.deactivate(self.console)
+            self.target.activate(self.power)
+            self.power.off()
+        elif status == Status.on:
+            self.transition(Status.off)
+            self.target.activate(self.console)
+            # cycle power
+            self.power.cycle()
+        elif status == Status.shell:
+            # transition to on
+            self.transition(Status.on)
+            self.target.activate(self.shell)
+            self.shell.run("uptime; uname -a")
+        elif status == Status.config:
+            self.transition(Status.shell)
+            self.target.activate(self.config)
+            self.config.configure()
+            self.target.deactivate(self.config)
+        else:
+            raise StrategyError(f"no transition found from {self.status} to {status}")
+        self.status = status
+
+    def force(self, status):
+        if not isinstance(status, Status):
+            status = Status[status]
+        if status == Status.off:
+            self.target.activate(self.power)
+        elif status == Status.on:
+            self.target.activate(self.on)
+        elif status == Status.shell:
+            self.target.activate(self.shell)
+        else:
+            raise StrategyError("can not force state {}".format(status))
+        self.status = status
