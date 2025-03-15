ba95c92563010bf5ec4d3ff423c5851103711c87
Perry Melange <isprotejesvalkata@gmail.com>
2025-03-15 13:02:47 +0100

    created test where a main target activates and uses a subtarget
    Signed-off-by: Perry Melange <isprotejesvalkata@gmail.com>

--- labgrid/strategy/test.py
+++ labgrid/strategy/test.py
@@ -4,11 +4,14 @@ from enum import auto
 
 import attr
 
+from labgrid import Environment, Target
 from labgrid.factory import target_factory
 from labgrid.strategy import Strategy, StrategyError
 from labgrid.util import Timeout
 from labgrid.util.ssh import sshmanager
 from labgrid.step import step
+from labgrid.resource import NetworkService
+from labgrid.driver import SSHDriver
 
 from time import sleep
 
@@ -16,13 +19,15 @@ class Status(enum.Enum):
     unknown = auto()
     off = auto()
     on = auto()
+    shell = auto()
 
 @target_factory.reg_driver
 @attr.s(eq=False)
 class TestStrategy(Strategy):
     """OpenWrtFlashBootStrategy - Strategy to boot from flash"""
     bindings = {
-    }
+        #"ssh": SSHDriver
+            }
 
     status = attr.ib(default=Status.unknown)
 
@@ -48,5 +53,22 @@ class TestStrategy(Strategy):
             self.transition(Status.off)
             print("Status on")
         
+        elif status == Status.shell:
+            env = self.target.env
+            target = env.get_target("dut")
+            target.activate(self, "dut")
+            #ns = target.get_resource("NetworkService", "altserv")
+            #target.bin_resource(ns)
+            ns = NetworkService(target, "altserv", "10.36.198.225", "root")
+            #target.bind_resource(ns)
+            [ubk] = SSHDriver.bindings
+            target.set_binding_map({ubk: "altserv"})
+            print(target.resources)
+            ssh = SSHDriver(target, "altserv")#.get_driver("SSHDriver", resource=ns)
+            target.activate(ssh)
+            result, _, _ = ssh.run("uname -a")
+            print(result)
+            pass
+
         self.status = status
 
