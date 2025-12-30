2a701525a0e88318509a1704cb2349c650341892
Perry Melange <isprotejesvalkata@gmail.com>
2025-12-30 15:09:34 +0100

    adding fixtures to pytest
    Signed-off-by: Perry Melange <isprotejesvalkata@gmail.com>

--- labgrid/tests/conftest.py
+++ labgrid/tests/conftest.py
@@ -1,18 +1,53 @@
 import pytest
 
-@pytest.fixture
-def shell_command(strategy):
+@pytest.fixture(scope="session")
+def shell_command(strategy, load_target):
     try:
         strategy.transition("shell")
         return strategy.shell
     except Exception:
         pytest.exit("Failed to transition to state shell", returncode=3)
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def configured(strategy, shell_command):
     try:
-        strategy.transition("confige")
+        strategy.transition("config")
         return shell_command
     except Exception:
-        pytest.exit("Failed to transition to state configure", returncode=3)
+        pytest.exit("Failed to transition to state config", returncode=3)
 
+@pytest.fixture(scope="session")
+@pytest.mark.lg_feature("ffwizard")
+def ffwizard(strategy, configured):
+    try:
+        strategy.transition("ffwizard")
+        return configured
+    except Exception:
+        pytest.exit("Failed to transition to state ffwizard", returncode=3)
+
+@pytest.fixture(scope="session")
+@pytest.mark.lg_feature("ramboot")
+def load_target(strategy):
+    try:
+        strategy.transition("ramboot")
+        return strategy
+    except Exception:
+        pytest.exit("Failed to transition to state ramboot", returncode=3)
+
+@pytest.fixture(scope="session")
+@pytest.mark.lg_feature("flash")
+def load_target(strategy):
+    try:
+        strategy.transition("flash")
+        return strategy
+    except Exception:
+        pytest.exit("Failed to transition to state flash", returncode=3)
+
+@pytest.fixture(scope="session")
+@pytest.mark.lg_feature("ramboot_then_flash")
+def load_target(strategy):
+    try:
+        strategy.transition("flash")
+        return strategy
+    except Exception:
+        pytest.exit("Failed to transition to state flash via ramboot", returncode=3)
--- labgrid/tests/test_olsr.py
+++ labgrid/tests/test_olsr.py
@@ -1,14 +1,9 @@
 import pytest
 
 @pytest.mark.lg_feature("olsr")
-def test_olsr(strategy, target):
-    strategy.transition("off")
-    strategy.transition("reset")
-    strategy.transition("off")
-    strategy.transition("ffwizard")
+def test_olsr(strategy, ffwizard):
 
     shell = strategy.shell #target.get_driver("SerialDriver")
-    target.activate(shell)
     pid, _, _ = shell.run("cat /tmp/run/olsrd.pid")
     _, _ ,running = shell.run(f"""test -d /proc/{pid[0]}""")
 
